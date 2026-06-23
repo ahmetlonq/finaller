e64751422338500ea70fbff19237b1b36914b2d5
ahmetlonq <ahmetlonq@gmail.com>
2026-06-23 15:46:41 +0300

    Create islem_sorulari.js

---
+++ resources/islem_sorulari.js
@@ -0,0 +1,12 @@
+const islemQuizData = [
+    {
+        question: "Aritmetik ortalaması 60, standart sapması 10 olan bir sınavda 80 puan alan öğrencinin Z puanı kaçtır?",
+        options: ["A) 1", "B) 2", "C) 3", "D) 0", "E) -1"],
+        correct: 1
+    },
+    {
+        question: "Z puanı 2 olan bir öğrencinin T puanı kaçtır?",
+        options: ["A) 60", "B) 70", "C) 80", "D) 50", "E) 40"],
+        correct: 1
+    }
+];
